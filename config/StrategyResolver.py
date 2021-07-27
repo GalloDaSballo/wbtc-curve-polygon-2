9ad65abc848997215e124cf9364207afa52a14a8
sajanrajdev <sajanrajdev10@gmail.com>
2021-07-27 17:32:10 -0400

    test: added harvest event display

--- config/StrategyResolver.py
+++ config/StrategyResolver.py
@@ -102,3 +102,23 @@ class StrategyResolver(StrategyCoreResolver):
         calls = self.add_entity_balances_for_tokens(calls, "wMATIC", wMATIC, entities)
 
         return calls
+
+    def confirm_harvest_state(self, before, after, tx):
+        key = "Harvest"
+        if key in tx.events:
+            assert len(tx.events[key]) == 1
+            event = tx.events[key][0]
+            keys = [
+                "harvested",
+                "crvAmount",
+            ]
+            for key in keys:
+                assert key in event
+
+            console.print("[blue]== harvest() State ==[/blue]")
+            self.printState(event, keys)
+
+    def printState(self, event, keys):
+        for key in keys:
+            print(key, ": ", event[key])
+
